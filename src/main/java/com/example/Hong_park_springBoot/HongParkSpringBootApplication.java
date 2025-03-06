784ea26620ff6c01c274ad8bccb30fe7b17f01b1
김재원 <wodnjs9058@naver.com>
2025-03-06 11:28:23 +0900

    prject will start

---
+++ src/main/java/com/example/Hong_park_springBoot/HongParkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hong_park_springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongParkSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongParkSpringBootApplication.class, args);
+	}
+
+}
