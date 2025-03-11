9ec8e8cd71cdeb527bc1422861c8462a811fb0b5
김재원 <wodnjs9058@naver.com>
2025-03-11 11:31:25 +0900

    comment list

--- src/main/java/com/example/Hong_park_springBoot/controller/ArticleController.java
+++ src/main/java/com/example/Hong_park_springBoot/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.Hong_park_springBoot.controller;
 
 import com.example.Hong_park_springBoot.dto.ArticleForm;
+import com.example.Hong_park_springBoot.dto.CommentDto;
 import com.example.Hong_park_springBoot.entity.Article;
 import com.example.Hong_park_springBoot.repository.ArticleRepository;
+import com.example.Hong_park_springBoot.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,6 +23,8 @@ public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    @Autowired
+    private CommentService commentService;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -48,8 +52,10 @@ public class ArticleController {
         log.info("id = " + id); // id를 잘 받았는지 확인하는 로그 찍기
         // 1. id를 조회해 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
+        List<CommentDto> commentsDtos = commentService.comments(id);
         // 2. 모델에 데이터 등록하기
         model.addAttribute("article", articleEntity);
+        model.addAttribute("commentDtos",commentsDtos);
         // 3. 뷰 페이지 반환하기
         return "articles/show";
     }
